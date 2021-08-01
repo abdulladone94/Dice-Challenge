35b99267ab87ee269e29372335771ebaca5f9d71
abdulladone94 <abdulladone94@gmail.com>
2021-08-01 19:36:19 +0530

    Random change player2

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const randomNumber1 = Math.floor((Math.random() * 6) + 1)
+const randomNumber2 = Math.floor((Math.random() * 6) + 1)
 const imgDice1 = 'dice'+randomNumber1+'.png'
+const imgDice2 = 'dice'+randomNumber2+'.png'
 const imgSource1 = 'images/'+imgDice1
+const imgSource2 = 'images/'+imgDice2
 
 document.querySelectorAll('img')[0].setAttribute('src', imgSource1)
-
+document.querySelectorAll('img')[1].setAttribute('src', imgSource2)
 
