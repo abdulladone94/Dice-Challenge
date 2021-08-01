6571458f223329103439b485bdab6113fd0954a8
abdulladone94 <abdulladone94@gmail.com>
2021-08-01 20:41:43 +0530

    End with winner

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const imgDice2 = 'dice'+randomNumber2+'.png'
 const imgSource1 = 'images/'+imgDice1
 const imgSource2 = 'images/'+imgDice2
 
-document.querySelectorAll('img')[0].setAttribute('src', imgSource1)
-document.querySelectorAll('img')[1].setAttribute('src', imgSource2)
+let no1 = document.querySelectorAll('img')[0].setAttribute('src', imgSource1)
+let no2 = document.querySelectorAll('img')[1].setAttribute('src', imgSource2)
 
+if (randomNumber1 < randomNumber2){
+    document.querySelector('h1').innerHTML = ('Player 2 Wins 🚩')
+}else if (randomNumber1 > randomNumber2){
+    document.querySelector('h1').innerHTML = ('🚩 Player 1 Wins')
+}else {
+    document.querySelector('h1').innerHTML = ('Drow!')
+}
